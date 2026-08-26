dfb49fc98a550675e224f66b273676ffc287aa4e
Truman Ho <132316164+Trumanchho@users.noreply.github.com>
2026-08-26 11:56:30 -0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hmap = defaultdict(list)
+        for s in strs:
+            letter_list = [0]*26
+            for c in s:
+                letter_list[ord(c)-ord('a')] += 1
+            hmap[tuple(letter_list)].append(s)
+        return list(hmap.values())
